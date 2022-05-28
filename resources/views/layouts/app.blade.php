eecfed029c5a54802d655c8bbf6cafd45df7363f
YoungDobrodum <yung.dobrodum@gmail.com>
2022-05-28 23:21:28 +0300

    design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
         body {
@@ -26,60 +26,66 @@
     </style>
 </head>
 <body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    ToDo List
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                    <li><a href="{{ route('task.index') }}">Tasks</a></li>
-
+    <nav class="navbar navbar-expand-lg bg-light ">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <a class="navbar-brand" href="{{ url('/') }}">ToDo List</a>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ url('/home') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('task.index') }}">Tasks</a>
+                    </li>
                 </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
+                <div class="d-flex" role="menuitem">
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/login') }}">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/register') }}">Register</a>
+                            </li>
+                        </ul>
 
                     @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </button>
+
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton1">
+                                <li><a class="dropdown-item" href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
-                        </li>
+                        </div>
                     @endif
-                </ul>
+                </div>
             </div>
         </div>
     </nav>
 
+
+
+
+
+
+
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+{{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>--}}
+{{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>--}}
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
         @if(count($tasks) === 0)
     <div class="panel panel-default">
         <div class="panel-heading">
@@ -8,53 +9,63 @@
         </div>
     </div>
         @endif
+
     @if(count($tasks)>0)
-    <div class="panel panel-default">
-
-        <div class="panel-heading">
-            All tasks
-        </div>
-
-        <div class="panel-body">
-            <table class="table table-striped task-table">
-
-                <thead>
-                    <tr>
-                        <th>Task</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-
-                @foreach($tasks as $task)
-                <tbody>
-                    <tr>
-                        <td class="table-text">
-                            <div>{{$task->name}}</div>
-                        </td>
-                        <td>
-                            <div style="display: flex">
-                                <form action="{{route('task.edit', $task->id)}}" method="POST">
-                                    {{csrf_field()}}
-                                    {{method_field('GET')}}
-
-                                    <button class="btn btn-secondary">Edit</button>
-                                </form>
-                                <form action="{{route('task.destroy', $task->id)}}" method="POST">
-                                    {{csrf_field()}}
-                                    {{method_field('DELETE')}}
-                                    <button class="btn btn-danger">Delete</button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-                @endforeach
-
-            </table>
+        <div class="container">
+        <div>
+            <h3>Dashboard</h3>
+            <figcaption class="blockquote-footer">
+                    These are all your tasks for today.
+            </figcaption>
 
         </div>
 
-    </div>
-    @endif
-    <a href="{{route('task.create')}}"><button class="btn btn-primary">Add new task</button></a>
+            <div class="panel panel-default">
+
+
+                <div class="panel-body">
+                    <table class="table table-hover">
+
+                        <thead>
+                        <tr>
+                            <th>Task</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+
+                        @foreach($tasks as $task)
+                            <tbody>
+                            <tr>
+                                <td class="table-text">
+                                    <div>{{$task->name}}</div>
+                                </td>
+                                <td>
+                                    <div style="display: flex">
+                                        <form action="{{route('task.edit', $task->id)}}" method="POST">
+                                            {{csrf_field()}}
+                                            {{method_field('GET')}}
+
+                                            <button class="btn btn-secondary">Edit</button>
+                                        </form>
+                                        <form action="{{route('task.destroy', $task->id)}}" method="POST">
+                                            {{csrf_field()}}
+                                            {{method_field('DELETE')}}
+                                            <button class="btn btn-danger">Delete</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        @endforeach
+
+                    </table>
+
+                </div>
+
+            </div>
+            @endif
+            <a href="{{route('task.create')}}"><button class="btn btn-primary">Add new task</button></a>
+        </div>
+
+
 @endsection
