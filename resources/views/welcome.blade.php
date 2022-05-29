10235cb0950bde8ddfa91114ed740dfa3b722f18
YoungDobrodum <yung.dobrodum@gmail.com>
2022-05-29 17:13:28 +0300

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container-fluid py-5">
-        <h1 class="display-5 fw-bold">Welcome</h1>
+        <h1 class="display-5 fw-bold">Welcome {{ Auth::user()->name }}</h1>
         <p class="col-md-8 fs-4">Dear friend, here you can add, edit or delete completed tasks. Just click the button below to get started.</p>
         <a href="{{route('task.create')}}"><button class="btn btn-primary btn-lg" type="button">Create my first task</button></a>
     </div>
